6b9e8b78c4e3e0d9cfbe6c74e06e1b43d2a1c0ff
Chad Skelton <cskeltonnews@gmail.com>
2019-06-26 11:27:34 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -143,7 +143,7 @@ def scrape_bcca(url):
         tweetit(record)
 
 def scrape_bcpc(url):
-        html = requests.get(url, cert=False)
+        html = requests.get(url, headers=('User-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'))
         # verify=False because getting 502 errors due to invalid certificate
         htmlpage = html.content
         
